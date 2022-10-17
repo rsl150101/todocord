066b9d02c10165ff9e58fa10b6336d3f2fe8481f
rsl150101 <dnwnd501@naver.com>
2022-10-17 19:30:32 +0900

    Add nonsense and Clean code
    Create variables.css
    Add main-screen part

--- src/js/login.js
+++ src/js/login.js
@@ -1,44 +1,54 @@
-const loginForm = document.getElementById("login-form");
-const loginFormTitle = document.getElementById("login-form__title");
-const loginFormUsername = document.getElementById("login-form__username");
-const loginFormBtn = document.getElementById("login-form__btn");
+const logInForm = document.getElementById("login-form");
+const logInFormTitle = document.getElementById("login-form__title");
+const logInFormUsername = document.getElementById("login-form__username");
+const logInFormBtn = document.getElementById("login-form__btn");
+const mainScreen = document.getElementById("main-screen");
+const logOut = document.getElementById("log-out");
 const HIDDEN = "hidden";
 
-document.body.classList.add("center");
+//* 로그인 함수
+function logIn(username) {
+  const icon = document.createElement("i");
+  logInFormTitle.classList.add(HIDDEN);
+  logInFormTitle.textContent = `${username} 님 환영합니다!`;
+  logInFormUsername.classList.add(HIDDEN);
+  logInForm.replaceChild(icon, logInFormUsername);
+  // 자식 노드 교체
+  icon.classList.add("fas", "fa-circle-notch", "login-form__icon");
+  setTimeout(() => {
+    icon.classList.remove("fa-cicle-notch");
+    icon.classList.add("fa-check", "icon--green");
+  }, 2000);
+  logInFormBtn.classList.add(HIDDEN);
+  logInFormBtn.removeEventListener("click", handleLoginBtn);
+  setTimeout(() => {
+    logInFormTitle.classList.remove(HIDDEN);
+  }, 2500);
+  setTimeout(() => {
+    logInForm.classList.add("form--ani");
+  }, 3500);
+  setTimeout(() => {
+    logInForm.remove();
+    mainScreen.classList.remove(HIDDEN);
+  }, 4500);
+}
 
 //* 버튼 클릭 함수
 function handleLoginBtn(event) {
   event.preventDefault();
-  const username = loginFormUsername.value;
+  const username = logInFormUsername.value;
   localStorage.setItem("username", username);
-  if (loginFormUsername.value === "") {
-    loginFormUsername.reportValidity();
+  if (logInFormUsername.value === "") {
+    logInFormUsername.reportValidity();
     // 유효성 검사 메시지 표시
   } else {
-    const icon = document.createElement("i");
-    loginFormTitle.classList.add(HIDDEN);
-    loginFormTitle.textContent = `${username} 님 환영합니다!`;
-    loginFormUsername.classList.add(HIDDEN);
-    loginForm.replaceChild(icon, loginFormUsername);
-    // 자식 노드 교체
-    icon.classList.add("fas", "fa-circle-notch", "login-form__icon");
-    setTimeout(() => {
-      icon.classList.remove("fa-cicle-notch");
-      icon.classList.add("fa-check", "icon--green");
-    }, 2000);
-    loginFormBtn.classList.add(HIDDEN);
-    loginFormBtn.removeEventListener("click", handleLoginBtn);
-    setTimeout(() => {
-      loginFormTitle.classList.remove(HIDDEN);
-    }, 2500);
-    setTimeout(() => {
-      loginForm.classList.add("form--ani");
-    }, 3500);
-    setTimeout(() => {
-      document.body.classList.remove("center");
-    }, 4500);
+    logIn(username);
   }
 }
 
+//* 로그아웃 함수
+function handleLogOut() {}
+
 //* 이벤트
-loginFormBtn.addEventListener("click", handleLoginBtn);
+logInFormBtn.addEventListener("click", handleLoginBtn);
+logOut.addEventListener("click", handleLogOut);
