c1014759aff0571eb24242d8f91e3154512be9f9
rsl150101 <dnwnd501@naver.com>
2022-10-22 22:59:47 +0900

    Update todo.js
    Added: to-do save
    
    Addition: Save to-do by date

--- src/js/todo.js
+++ src/js/todo.js
@@ -2,33 +2,52 @@ const todoUl = document.getElementById("todo-list");
 const todoForm = document.getElementById("todo-form");
 const todoFormInput = todoForm.querySelector('input[type="text"]');
 
+const savedTodo = localStorage.getItem("todos");
+
+let todoArr = [];
+
 //* 해당 li 체크박스 변경 및 밑줄 toggle
 function handleCheckBtn(event) {
+  const li = event.target.parentElement;
+  const liIndex = todoArr.findIndex((item) => item.id == li.id);
   if (event.target.innerText === "⬜") {
     event.target.innerText = "✅";
     event.target.nextSibling.classList.toggle("text--line-through");
+    todoArr[liIndex].check = true;
   } else {
     event.target.innerText = "⬜";
     event.target.nextSibling.classList.toggle("text--line-through");
+    todoArr[liIndex].check = false;
   }
+  saveTodo();
 }
 
 //* 해당 li 삭제 함수
 function handleDeleteBtn(event) {
-  event.target.parentElement.remove();
+  const li = event.target.parentElement;
+  li.remove();
+  todoArr = todoArr.filter((item) => item.id != li.id);
+  saveTodo();
 }
 
 //* 입력된 값 실제 화면에 나타내는 함수
-function paintTodo(todoText) {
+function paintTodo(todoObj) {
   const todoLi = document.createElement("li");
   const todoLiSpan = document.createElement("span");
   const checkBtn = document.createElement("button");
   const deleteBtn = document.createElement("button");
 
-  checkBtn.textContent = "⬜";
-  todoLiSpan.textContent = todoText;
+  if (todoObj.check) {
+    checkBtn.textContent = "✅";
+    todoLiSpan.classList.add("text--line-through");
+  } else {
+    checkBtn.textContent = "⬜";
+  }
+
+  todoLiSpan.textContent = todoObj.text;
   deleteBtn.textContent = "🗑️";
   todoUl.appendChild(todoLi);
+  todoLi.id = todoObj.id;
   todoLi.appendChild(checkBtn);
   todoLi.appendChild(todoLiSpan);
   todoLi.appendChild(deleteBtn);
@@ -37,12 +56,30 @@ function paintTodo(todoText) {
   deleteBtn.addEventListener("click", handleDeleteBtn);
 }
 
+function saveTodo() {
+  localStorage.setItem("todos", JSON.stringify(todoArr));
+}
+
 //* 입력 값 제출하는 함수
 function handleTodoSubmit(event) {
   event.preventDefault();
-  const todoText = todoFormInput.value;
+  const todoObj = {
+    username,
+    text: todoFormInput.value,
+    id: Date.now(),
+    check: false,
+  };
+  todoArr.push(todoObj);
   todoFormInput.value = "";
-  paintTodo(todoText);
+  paintTodo(todoObj);
+  saveTodo();
+}
+
+//* 기존의 to-do 목록이 있으면 나타내기
+if (savedTodo !== null) {
+  const todoObj = JSON.parse(savedTodo);
+  todoArr = todoObj;
+  todoObj.forEach(paintTodo);
 }
 
 //* 이벤트
